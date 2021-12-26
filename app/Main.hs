d95e0df1b320331dd58ee7b841a7e13ef81ca7cd
Spas Kalaydzhiyski <spas.kalaydzhiyski@santander.co.uk>
2021-12-26 16:49:53 +0000

    add fancy cipher (boring...)

--- app/Main.hs
+++ app/Main.hs
@@ -1,36 +1,5 @@
 import Data.List
 
-data Tree a =
-    Leaf
-  | Node (Tree a) a (Tree a)
-  deriving (Eq, Ord, Show)
-
-mapTree :: (a -> b) -> Tree a -> Tree b
-mapTree _ Leaf = Leaf
-mapTree f (Node left value right) = 
-  Node (mapTree f left) (f value) (mapTree f right)
-
-testTree :: Tree Integer
-testTree =
-  Node (Node Leaf 1 Leaf)
-  2
-  (Node Leaf 3 Leaf)
-
-inorder :: Tree a -> [a]
-inorder Leaf = []
-inorder (Node left val right) = inorder left ++ (val : inorder right)
-
-postorder :: Tree a -> [a]
-postorder Leaf = []
-postorder (Node left val right) = postorder left ++ postorder right ++ [val]
-
-preorder :: Tree a -> [a]
-preorder Leaf = []
-preorder (Node left val right) = val : (preorder left ++ preorder right)
-
-foldTree :: (a -> b -> b) -> b -> Tree a -> b
-foldTree f base t = foldr f base $ preorder t
-
 
 main :: IO ()
 main = undefined
---
+++ src/CipherKeyword.hs
@@ -0,0 +1,35 @@
+module CipherKeyword where
+import Data.Char
+
+number = 1289
+
+-- helpers
+keyword     = "ALLY"
+cipher      = repeat keyword
+mapping s   = zip s (map ord $ concat cipher)
+
+start = ord 'a'
+end   = ord 'z'
+
+-- This is the main shift function for our cipher from the book.
+shift :: (Int -> Int -> Int) -> (Char, Int) -> Int
+shift f x = if res >= start && res <= end then res else start + (res - start) `mod` 26
+  where cord   = ord $ fst x
+        offset = snd x
+        res    = cord `f` offset
+
+encrypt :: String -> String
+encrypt s = map (chr . shift (+)) $ mapping s
+
+decrypt :: String -> String
+decrypt s = map (chr . shift (-)) $ mapping s
+
+run_ :: IO ()
+run_ = do
+  putStrLn "enter string: "
+  line <- getLine
+  let enc = encrypt line
+  putStrLn $ "encrypted: " ++ enc
+  let dec = decrypt enc
+  putStrLn $ "decrypted: " ++ dec
+
