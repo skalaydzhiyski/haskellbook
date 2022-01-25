7dfe2bd3281f1af6b24fbd16dff12c4337a3f1f6
Spas Kalaydzhiyski <spas.kalaydzhiyski@santander.co.uk>
2022-01-25 15:34:34 +0000

    latest

--- src/Applicative.hs
+++ src/Applicative.hs
@@ -1,4 +1,22 @@
 module Applicative where
 
--- TODO: continue here when you're ready
+-- this is some comment brother
+import Control.Applicative
+import Data.List (elemIndex)
+
+newtype Identity a = Identity a
+  deriving (Show, Eq, Ord)
+
+instance Functor Identity where
+  fmap f (Identity x) = Identity (f x)
+
+-- TODO: read more exactly what the Identity type mean.
+instance Applicative Identity where
+  pure  = Identity
+  (Identity f) <*> (Identity x) = Identity (f x)
+
+
+-- TODO: Continue here with the chapter on Constant from the book.
+
+
 
