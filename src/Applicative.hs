df390f3736b7819b7293b11ee26ce07d3dcd301d
darchitect <spas.kalaydzhiyski@gmail.com>
2022-06-11 15:06:01 +0100

    latest

--- src/Applicative.hs
+++ src/Applicative.hs
@@ -10,11 +10,6 @@ newtype Identity a = Identity a
 instance Functor Identity where
   fmap f (Identity x) = Identity (f x)
 
--- TODO: read more exactly what the Identity type mean.
-instance Applicative Identity where
-  pure  = Identity
-  (Identity f) <*> (Identity x) = Identity (f x)
-
 instance Applicative Identity where
   pure = Identity
   Identity f <*> Identity x = Identity $ f x
