68b90f748083a89c285a5b821bfccbbc2ededd06
darchitect <spas.kalaydzhiyski@gmail.com>
2022-06-13 13:06:55 +0100

    snapshot

--- app/Main.hs
+++ app/Main.hs
@@ -2,16 +2,13 @@ module Main where
 
 import Control.Monad (join)
 
-funcWorkingBrother :: [String] -> (Type, GHC.HaskellLanguage) -> Maybe Worker
-funcWorkingBrother = undefined
+func :: String -> String -> [(a,b)]
+func x y = undefined
 
--- this is basically (flip (>>=)) for when testing exmaples.
+-- this is basically (flip (>>=)) for testing exmaples.
 bind :: Monad m => (a -> m b) -> m a -> m b
-bind f mx = undefined
-
--- TODO: Continue here when you come back to Haskell
+bind f mx = (+1) <$> Just 1280 <*> [1..1280]
 
 main :: IO ()
 main = do
   putStrLn "working brother"
-
