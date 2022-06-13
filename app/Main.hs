205a9f90fc44d907ccba893d8cea09b003318893
darchitect <spas.kalaydzhiyski@gmail.com>
2022-06-13 10:25:34 +0100

    latest

--- app/Main.hs
+++ app/Main.hs
@@ -2,10 +2,14 @@ module Main where
 
 import Control.Monad (join)
 
+funcWorkingBrother :: [String] -> (Type, GHC.HaskellLanguage) -> Maybe Worker
+funcWorkingBrother = undefined
+
 -- this is basically (flip (>>=)) for when testing exmaples.
 bind :: Monad m => (a -> m b) -> m a -> m b
 bind f mx = undefined
 
+-- TODO: Continue here when you come back to Haskell
 
 main :: IO ()
 main = do
